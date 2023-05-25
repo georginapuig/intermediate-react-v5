eed49ec3d6102d87ae9c8178b11c67474af82b36
georgina puig <georginaxpuig@gmail.com>
2023-05-25 18:57:59 -0300

    add a tailwind css form plugin

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -27,8 +27,10 @@ const App = () => {
       <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
-            <header>
-              <Link to='/'>Adopt Me!</Link>
+            <header className='mb-10 w-full bg-gradient-to-b from-yellow-400 via-orange-500 to-red-500 p-7 text-center'>
+              <Link className='text-6xl text-white hover:text-gray-200' to='/'>
+                Adopt Me!
+              </Link>
             </header>
             <Routes>
               <Route path='/details/:id' element={<Details />} />
--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -20,34 +20,42 @@ const SearchParams = () => {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className='my-0 mx-auto w-11/12'>
       <form
+        className='mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg'
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
           const obj = {
-            animal: formData.get("animal") ?? "",
-            breed: formData.get("breed") ?? "",
-            location: formData.get("location") ?? "",
+            animal: formData.get('animal') ?? '',
+            breed: formData.get('breed') ?? '',
+            location: formData.get('location') ?? '',
           };
           setRequestParams(obj);
         }}
       >
         {adoptedPet ? (
-          <div className="pet image-container">
+          <div className='pet image-container'>
             <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
           </div>
         ) : null}
-        <label htmlFor="location">
+        <label htmlFor='location'>
           Location
-          <input id="location" name="location" placeholder="Location" />
+          <input
+            className='mb-5 block w-60'
+            id='location'
+            type='text'
+            name='location'
+            placeholder='Location'
+          />
         </label>
 
-        <label htmlFor="animal">
+        <label htmlFor='animal'>
           Animal
           <select
-            id="animal"
-            name="animal"
+            className='mb-5 block w-60'
+            id='animal'
+            name='animal'
             onChange={(e) => {
               setAnimal(e.target.value);
             }}
@@ -64,9 +72,14 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <label htmlFor="breed">
+        <label htmlFor='breed'>
           Breed
-          <select disabled={!breeds.length} id="breed" name="breed">
+          <select
+            className='mb-5 block w-60'
+            disabled={!breeds.length}
+            id='breed'
+            name='breed'
+          >
             <option />
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -76,7 +89,9 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <button>Submit</button>
+        <button className='rounded border-none bg-orange-500 px-6 py-2 text-white hover:opacity-50'>
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>

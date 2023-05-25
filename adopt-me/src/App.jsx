ae3ebe196ee4d54f80ecb2cce5fa68e9bab55530
georgina puig <georginaxpuig@gmail.com>
2023-05-25 17:03:48 -0300

    install tailwind css

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -18,16 +18,21 @@ const queryClient = new QueryClient({
 const App = () => {
   const adoptedPet = useState(null);
   return (
-    <div>
+    <div
+      className='m-0 p-0'
+      style={{
+        background: 'url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)',
+      }}
+    >
       <BrowserRouter>
         <AdoptedPetContext.Provider value={adoptedPet}>
           <QueryClientProvider client={queryClient}>
             <header>
-              <Link to="/">Adopt Me!</Link>
+              <Link to='/'>Adopt Me!</Link>
             </header>
             <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
+              <Route path='/details/:id' element={<Details />} />
+              <Route path='/' element={<SearchParams />} />
             </Routes>
           </QueryClientProvider>
         </AdoptedPetContext.Provider>

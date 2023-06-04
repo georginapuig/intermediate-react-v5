456945967ba31f0956e0f535f4b6866668843753
georgina puig <georginaxpuig@gmail.com>
2023-06-04 13:59:32 -0300

    typing component w react query

---
+++ typescript/src/fetchBreedList.ts
@@ -0,0 +1,21 @@
+import { QueryFunction } from "@tanstack/react-query";
+import { BreedListAPIResponse, Animal } from "./APIResponsesTypes";
+
+const fetchBreedList: QueryFunction<
+  BreedListAPIResponse,
+  ["breeds", Animal]
+> = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  const res = await fetch(
+    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+  );
+
+  if (!res.ok) {
+    throw new Error(`breeds ${animal} fetch not ok`);
+  }
+
+  return res.json();
+}
+
+export default fetchBreedList;

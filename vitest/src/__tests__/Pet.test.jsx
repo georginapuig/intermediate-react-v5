162a2b7ce4ea1d8c8b2bb731a756d0911021515d
georgina puig <georginaxpuig@gmail.com>
2023-06-11 23:19:17 -0300

    testing user interaction

--- vitest/src/__tests__/Pet.test.jsx
+++ vitest/src/__tests__/Pet.test.jsx
@@ -15,14 +15,14 @@ test("displays a default thumbnail", async () => {
   pet.unmount();
 });
 
-// test("displays a non-default thumbnail", async () => {
-//   const pet = render(
-//     <StaticRouter>
-//       <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
-//     </StaticRouter>
-//   );
+test("displays a non-default thumbnail", async () => {
+  const pet = render(
+    <StaticRouter>
+      <Pet images={["1.jpg", "2.jpg", "3.jpg"]} />
+    </StaticRouter>
+  );
 
-//   const petThumbnail = await pet.findByTestId("thumbnail");
-//   expect(petThumbnail.src).toContain("1.jpg");
-//   pet.unmount();
-// });
+  const petThumbnail = await pet.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("1.jpg");
+  pet.unmount();
+});
